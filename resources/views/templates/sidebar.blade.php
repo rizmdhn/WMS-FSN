810d1ab9604edee5d936cc3b6f615ff0273c8854
RizkiR <rizkir42@gmail.com>
2022-05-29 22:24:27 +0700

    role

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -2,16 +2,17 @@
   <div class="sidebar-heading">Sistem Pergudangan </div>
   <div class="list-group list-group-flush">
     <a href="{{ url('/home') }}" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-    <a href="{{ route('employee.index') }}" class="list-group-item list-group-item-action bg-light">Data Karyawan</a>
-    <a href="{{ route('supplier.index') }}" class="list-group-item list-group-item-action bg-light">Seksi Pemasok</a>
     <a href="{{ route('product.index') }}" class="list-group-item list-group-item-action bg-light">Data Barang</a>
     <a href="{{ route('sell.index') }}" class="list-group-item list-group-item-action bg-light">Barang Diambil</a>
     <a href="{{ route('purchase.index') }}" class="list-group-item list-group-item-action bg-light">Barang Masuk</a>
     <a href="{{ route('report.index') }}" class="list-group-item list-group-item-action bg-light">Laporan Pengambilan</a>
     <a href="{{ route('report2.index') }}" class="list-group-item list-group-item-action bg-light">Laporan Barang Masuk</a>
-
     <!-- yang hanya bisa diakses admin -->
     @if(Auth::user()->akses == 'admin')
+    <a href="{{ route('gudang.index') }}" class="list-group-item list-group-item-action bg-light">Gudang</a>
+    <a href="{{ route('employee.index') }}" class="list-group-item list-group-item-action bg-light">Data Karyawan</a>
+    <a href="{{ route('supplier.index') }}" class="list-group-item list-group-item-action bg-light">Seksi Pemasok</a>
+
     <li class="nav-item dropdown" style="list-style-type: none;">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: black;">
         Database
@@ -20,11 +21,8 @@
         <a class="dropdown-item" href="{{ route('category.index') }}"></i>Kategori Barang</a>
         <a class="dropdown-item" href="{{ route('unit.index') }}"></i>Satuan</a>
         <a class="dropdown-item" href="{{ route('user.index') }}">User</a>
-        <a class="dropdown-item" href="{{ route('gudang.index') }}">Gudang</a>
       </div>
     </li>
     @endif
-
-    <a href="{{ url('/about') }}" class="list-group-item list-group-item-action bg-light">About</a>
   </div>
 </div>
